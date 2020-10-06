235bc01b5c2c1c2b3398e7e8caf197eaa95ad30a
Almandeel <moezsapry2022@gmail.com>
2020-10-06 18:22:20 +0200

    fix bug

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -14,34 +14,40 @@ class CompanyController extends Controller
     }
 
     public function newOrders() {
-        $orders = Order::where('status', Order::ORDER_ACCEPTED)->get()->map(function($order) {
-            return [
-                'id'            => $order->id,
-                'order_type'    => $order->type,
-                'from'          => $order->from,
-                'to'            => $order->to,
-            ];
-        });
-
-        return response()->json($orders); 
+        if(auth('api')->user()->company_id) {
+            $orders = Order::where('status', Order::ORDER_ACCEPTED)->get()->map(function($order) {
+                return [
+                    'id'            => $order->id,
+                    'order_type'    => $order->type,
+                    'from'          => $order->from,
+                    'to'            => $order->to,
+                ];
+            });
+            return response()->json($orders); 
+        }else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
     }
 
     public function oldOrders() {
-        $orders = Order::with('items')->where('company_id', auth('api')->user()->company_id)->get()->map(function($order) {
-            return [
-                'id'            => $order->id,
-                'order_type'    => $order->type,
-                'from'          => $order->from,
-                'to'            => $order->to,
-                'status'        => __('global.' . Order::status[$order->status])
-            ];
-        });
-
-
-        return response()->json([$orders]); 
+        if(auth('api')->user()->company_id) {
+            $orders = Order::with('items')->where('company_id', auth('api')->user()->company_id)->get()->map(function($order) {
+                return [
+                    'id'            => $order->id,
+                    'order_type'    => $order->type,
+                    'from'          => $order->from,
+                    'to'            => $order->to,
+                    'status'        => __('global.' . Order::status[$order->status])
+                ];
+            });
+            return response()->json([$orders]); 
+        }else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
     }
 
     public function showOrder(Request $request) {
+        if(auth('api')->user()->company_id) {
         $order = Order::with('items')->where('id', $request->order_id)->where('company_id', auth('api')->user()->company_id)->get()->map(function ($order) {
             return [
                 'id'            => $order->id,
@@ -54,13 +60,16 @@ class CompanyController extends Controller
                 'status'        => __('global.' . Order::status[$order->status])
             ];
         });
-
         $tender = OrderTender::where('order_id', $request->order_id)->where('company_id', auth()->user()->company_id)->first();
-
         return response()->json([
             'order'     => $order,
             'tender'    => $tender
         ]);
+        }else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+
     }
 
     public function updateOrder(Request $request) {
