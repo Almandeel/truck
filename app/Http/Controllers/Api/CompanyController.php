9743723a05e8f2f88321ddc4dc87fcce24c11531
Almandeel <moezsapry2022@gmail.com>
2020-10-06 18:34:49 +0200

    fix bug

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -48,28 +48,26 @@ class CompanyController extends Controller
 
     public function showOrder(Request $request) {
         if(auth('api')->user()->company_id) {
-        $order = Order::with('items')->where('id', $request->order_id)->where('company_id', auth('api')->user()->company_id)->get()->map(function ($order) {
-            return [
-                'id'            => $order->id,
-                'from'          => $order->from,
-                'to'            => $order->to,
-                'order_type'    => $order->type,
-                'shipping_date' => $order->shipping_date,
-                'savior_name'   => $order->savior_name,
-                'savior_phone'  => $order->savior_phone,
-                'status'        => __('global.' . Order::status[$order->status])
-            ];
-        });
-        $tender = OrderTender::where('order_id', $request->order_id)->where('company_id', auth()->user()->company_id)->first();
-        return response()->json([
-            'order'     => $order,
-            'tender'    => $tender
-        ]);
+            $order = Order::with('items')->where('id', $request->order_id)->get()->map(function ($order) {
+                return [
+                    'id'            => $order->id,
+                    'from'          => $order->from,
+                    'to'            => $order->to,
+                    'order_type'    => $order->type,
+                    'shipping_date' => $order->shipping_date,
+                    'savior_name'   => $order->savior_name,
+                    'savior_phone'  => $order->savior_phone,
+                    'status'        => __('global.' . Order::status[$order->status])
+                ];
+            });
+            $tender = OrderTender::where('order_id', $request->order_id)->where('company_id', auth()->user()->company_id)->first();
+            return response()->json([
+                'order'     => $order,
+                'tender'    => $tender
+            ]);
         }else {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
-
     }
 
     public function updateOrder(Request $request) {
